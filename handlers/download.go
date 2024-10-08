1b907ebaeff617bffee8f7fcad2b8b4767264950
balasol <balaji.sol@solulab.co>
2024-10-08 17:33:02 +0530

    Added Swagger API documentation

--- handlers/download.go
+++ handlers/download.go
@@ -9,6 +9,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// DownloadFile godoc
+// @Summary Download a merged file by file ID
+// @Description Download the full file by merging its parts using the file ID.
+// @Tags file
+// @Produce application/octet-stream
+// @Param fileID query string true "Unique file ID"
+// @Success 200 {file} file "Downloaded file"
+// @Failure 400 {string} string "fileID is required"
+// @Router /download [get]
 func DownloadFile(c *fiber.Ctx) error {
 	fileID := c.Query("fileID")
 	if fileID == "" {
